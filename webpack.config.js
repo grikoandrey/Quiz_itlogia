bf907bf95249ee7fb7a8d3ceda654654f6cc1564
grikoandrey <griko.and@gmail.com>
2024-11-07 11:29:44 +0100

    mod_9-13 quiz part_3 & babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/app.js',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -22,4 +23,21 @@ module.exports = {
             ],
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        // targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ]
+    }
 };
