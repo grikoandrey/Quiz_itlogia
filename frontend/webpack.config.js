35f2070f449d050cb8d5ad01bfbd43d1f3d3c90c
grikoandrey <griko.and@gmail.com>
2025-01-10 10:32:52 +0100

    mod_11-6 Quiz to TS with home ex

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/app.ts',
     mode: 'development',
     output: {
         filename: 'main.js',
